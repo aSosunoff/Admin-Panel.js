f8e1e4a76b58208c5f6c3759472156ff3fe28f6a
asosunoff <al.sosunoff@gmail.com>
2020-05-04 17:21:52 +0300

    folder transfer

--- src/components/page/sales/table/index.js
+++ src/components/page/sales/table/index.js
@@ -1,3 +1,4 @@
+import { HTMLBulder } from '../../../../utils/HTMLBulder.js';
 import { TableInfinityServer } from '../../../common/table/table-infinity-server/index.js';
 
 export class TableSales extends TableInfinityServer {
@@ -8,5 +9,28 @@ export class TableSales extends TableInfinityServer {
 			pageSize,
 			urlQueryPerem,
 		});
+	}
+
+	/**@override*/
+	renderNextRows(data = []) {
+		if (!data.length) {
+			return;
+		}
+
+		const cells = this.headersConfig.map(({ id, template }) => ({
+			id,
+			template,
+		}));
+
+		const templateDefault = data => `<div class="sortable-table__cell">${data}</div>`;
+
+		this.subElements.body.append(
+			...data.map(data =>
+				HTMLBulder.getElementFromString(`
+				<div class="sortable-table__row">
+					${cells.map(({ id, template = templateDefault }) => template(data[id])).join('')}
+				</div>`),
+			),
+		);
 	}
 }
